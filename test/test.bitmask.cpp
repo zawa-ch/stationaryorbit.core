ed4cb3c0e7e6378d454433797469cf6857e919e5
zawa-ch <satellite.2e1834097@gmail.com>
2021-04-15 23:18:49 +0900

    * test.bitmask
    細かい単位でテスト実行するよう修正

--- test/test.bitmask.cpp
+++ test/test.bitmask.cpp
@@ -20,9 +20,37 @@
 #include "stationaryorbit/core.bitoperation.hpp"
 using namespace zawa_ch::StationaryOrbit;
 
+int test1();
+int test2();
+int test3();
+int test4();
+
 int main(int argc, char const *argv[])
 {
 	std::cout << "<--- BitMask --->" << std::endl;
+	if (argc < 2)
+	{
+		std::cerr << "E: At least 1 argument is required";
+		return 2;
+	}
+
+	auto test_index = std::stoi(argv[1]);
+	switch(test_index)
+	{
+		case 1: { return test1(); }
+		case 2: { return test2(); }
+		case 3: { return test3(); }
+		case 4: { return test4(); }
+		default:
+		{
+			std::cerr << "Invalid test index";
+			return 2;
+		}
+	}
+}
+
+int test1()
+{
 	auto mask = BitMask<uint32_t>(0xF0);
 	std::cout << "mask: 0xF0" << std::endl;
 
@@ -30,50 +58,65 @@ int main(int argc, char const *argv[])
 	if (mask.GetFrom(0x38) == 0x30)
 	{
 		std::cout << "...OK" << std::endl;
+		return 0;
 	}
 	else
 	{
 		std::cout << "...NG" << std::endl;
-		throw std::runtime_error("test 1 failed");
+		return 1;
 	}
+}
+
+int test2()
+{
+	auto mask = BitMask<uint32_t>(0xF0);
+	std::cout << "mask: 0xF0" << std::endl;
 
 	std::cout << "2. mask.GetAlignedFrom(0x38) ->0x03?";
 	if (mask.GetAlignedFrom(0x38) == 0x03)
 	{
 		std::cout << "...OK" << std::endl;
+		return 0;
 	}
 	else
 	{
 		std::cout << "...NG" << std::endl;
-		throw std::runtime_error("test 2 failed");
+		return 1;
 	}
+}
 
-	mask = BitMask<uint32_t>(0x5555);
+int test3()
+{
+	auto mask = BitMask<uint32_t>(0x5555);
 	std::cout << "mask: 0x5555" << std::endl;
 
 	std::cout << "3. mask.GetFrom(0x250) ->0x050?";
 	if (mask.GetFrom(0x250) == 0x50)
 	{
 		std::cout << "...OK" << std::endl;
+		return 0;
 	}
 	else
 	{
 		std::cout << "...NG" << std::endl;
-		throw std::runtime_error("test 3 failed");
+		return 1;
 	}
+}
 
-	mask = BitMask<uint32_t>(0x040);
+int test4()
+{
+	auto mask = BitMask<uint32_t>(0x040);
 	std::cout << "mask: 0x0040" << std::endl;
 
 	std::cout << "4. mask.GetFrom(0x250) ->0x01?";
 	if (mask.GetAlignedFrom(0x250) == 0x1)
 	{
 		std::cout << "...OK" << std::endl;
+		return 0;
 	}
 	else
 	{
 		std::cout << "...NG" << std::endl;
-		throw std::runtime_error("test 4 failed");
+		return 1;
 	}
-	return 0;
 }
