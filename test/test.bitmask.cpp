2ab8f529733ec9ada61c46e5cb9978cca529018d
zawa-ch <satellite.2e1834097@gmail.com>
2021-04-25 22:05:56 +0900

    * stationaryorbit.core.test.bitmask

--- test/test.bitmask.cpp
+++ test/test.bitmask.cpp
@@ -17,106 +17,118 @@
 //	If not, see <http://www.gnu.org/licenses/>.
 //
 #include <iostream>
+#include <array>
+#include <functional>
 #include "stationaryorbit/core.bitoperation.hpp"
 using namespace zawa_ch::StationaryOrbit;
 
-int test1();
-int test2();
-int test3();
-int test4();
+constexpr int check_if(bool condition)
+{
+	if (condition)
+	{
+		std::cout << "...OK" << std::endl;
+		return 0;
+	}
+	else
+	{
+		std::cout << "...NG" << std::endl;
+		return 1;
+	}
+}
+
+std::array<std::function<int(void)>, 4> tests =
+{
+	[]()
+	{
+		auto mask = BitMask<uint32_t>(0xF0);
+		std::cout << "mask: 0xF0" << std::endl;
+
+		std::cout << "1. mask.GetFrom(0x38) ->0x30?";
+		if (mask.GetFrom(0x38) == 0x30)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		auto mask = BitMask<uint32_t>(0xF0);
+		std::cout << "mask: 0xF0" << std::endl;
+
+		std::cout << "2. mask.GetAlignedFrom(0x38) ->0x03?";
+		if (mask.GetAlignedFrom(0x38) == 0x03)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		auto mask = BitMask<uint32_t>(0x5555);
+		std::cout << "mask: 0x5555" << std::endl;
+
+		std::cout << "3. mask.GetFrom(0x250) ->0x050?";
+		if (mask.GetFrom(0x250) == 0x50)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		auto mask = BitMask<uint32_t>(0x040);
+		std::cout << "mask: 0x0040" << std::endl;
+
+		std::cout << "4. mask.GetFrom(0x250) ->0x01?";
+		if (mask.GetAlignedFrom(0x250) == 0x1)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+};
 
 int main(int argc, char const *argv[])
 {
 	std::cout << "<--- BitMask --->" << std::endl;
-	if (argc < 2)
+	int test_index;
+	if (2 <= argc)
 	{
-		std::cerr << "E: At least 1 argument is required";
+		test_index = std::stoi(argv[1]);
+	}
+	else
+	{
+		std::cerr << "W: No test# specified" << std::endl;
+		std::cout << "Test#? ";
+		std::cin >> test_index;
+	}
+	if (0 < test_index && test_index <= tests.size() )
+	{
+		return tests[test_index - 1]();
+	}
+	else
+	{
+		std::cerr << "E: Invalid test index" << std::endl;
 		return 2;
-	}
-
-	auto test_index = std::stoi(argv[1]);
-	switch(test_index)
-	{
-		case 1: { return test1(); }
-		case 2: { return test2(); }
-		case 3: { return test3(); }
-		case 4: { return test4(); }
-		default:
-		{
-			std::cerr << "Invalid test index";
-			return 2;
-		}
-	}
-}
-
-int test1()
-{
-	auto mask = BitMask<uint32_t>(0xF0);
-	std::cout << "mask: 0xF0" << std::endl;
-
-	std::cout << "1. mask.GetFrom(0x38) ->0x30?";
-	if (mask.GetFrom(0x38) == 0x30)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test2()
-{
-	auto mask = BitMask<uint32_t>(0xF0);
-	std::cout << "mask: 0xF0" << std::endl;
-
-	std::cout << "2. mask.GetAlignedFrom(0x38) ->0x03?";
-	if (mask.GetAlignedFrom(0x38) == 0x03)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test3()
-{
-	auto mask = BitMask<uint32_t>(0x5555);
-	std::cout << "mask: 0x5555" << std::endl;
-
-	std::cout << "3. mask.GetFrom(0x250) ->0x050?";
-	if (mask.GetFrom(0x250) == 0x50)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test4()
-{
-	auto mask = BitMask<uint32_t>(0x040);
-	std::cout << "mask: 0x0040" << std::endl;
-
-	std::cout << "4. mask.GetFrom(0x250) ->0x01?";
-	if (mask.GetAlignedFrom(0x250) == 0x1)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
 	}
 }
