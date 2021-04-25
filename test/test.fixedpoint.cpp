45b799f80368f63ae49975c83cde7fbc4e962f31
zawa-ch <satellite.2e1834097@gmail.com>
2021-04-25 22:08:08 +0900

    * stationaryorbit.core.test.fixedpoint

--- test/test.fixedpoint.cpp
+++ test/test.fixedpoint.cpp
@@ -17,328 +17,297 @@
 //	If not, see <http://www.gnu.org/licenses/>.
 //
 #include <iostream>
+#include <array>
+#include <functional>
 #include "stationaryorbit/core.numeral.hpp"
 using namespace zawa_ch::StationaryOrbit;
 
-int test1();
-int test2();
-int test3();
-int test4();
-int test5();
-int test6();
-int test7();
-int test8();
-int test9();
-int test10();
-int test11();
-int test12();
-int test13();
-int test14();
-int test15();
-int test16();
-int test17();
-int test18();
+constexpr int check_if(bool condition)
+{
+	if (condition)
+	{
+		std::cout << "...OK" << std::endl;
+		return 0;
+	}
+	else
+	{
+		std::cout << "...NG" << std::endl;
+		return 1;
+	}
+}
+
+std::array<std::function<int(void)>, 18> tests =
+{
+	[]()
+	{
+		std::cout << "1. Type FixedPoint8q7_t meets the traits IsNumericalType?";
+		if (Traits::IsNumericalType<FixedPoint8q7_t>)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		std::cout << "2. Type FixedPoint16q8_t meets the traits IsNumericalType?";
+		if (Traits::IsNumericalType<FixedPoint16q8_t>)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		std::cout << "3. Type FixedPoint16q15_t meets the traits IsNumericalType?";
+		if (Traits::IsNumericalType<FixedPoint16q15_t>)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		std::cout << "4. Type FixedPoint32q16_t meets the traits IsNumericalType?";
+		if (Traits::IsNumericalType<FixedPoint32q16_t>)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		std::cout << "5. Type FixedPoint32q31_t meets the traits IsNumericalType?";
+		if (Traits::IsNumericalType<FixedPoint32q31_t>)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		std::cout << "6. Type FixedPoint64q32_t meets the traits IsNumericalType?";
+		if (Traits::IsNumericalType<FixedPoint64q32_t>)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		std::cout << "7. Type FixedPoint64q63_t meets the traits IsNumericalType?";
+		if (Traits::IsNumericalType<FixedPoint64q63_t>)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		auto f1 = FixedPoint16q8_t(16);
+		std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
+		std::cout << "8. f1 == 16 ->true?";
+		if (double(f1) == 16)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		auto f2 = FixedPoint16q8_t(0.125);
+		std::cout << "f2 = FixedPoint16q8_t(0.125)" << std::endl;
+		std::cout << "9. f2 == 0.125 ->true?";
+		if (double(f2) == 0.125)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		auto f1 = FixedPoint16q8_t(16);
+		auto f2 = FixedPoint16q8_t(0.125);
+		std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
+		std::cout << "f2 = FixedPoint16q8_t(0.125)" << std::endl;
+		std::cout << "10. f1 + f2 == 16.125 ->true?";
+		if (double(f1 + f2) == 16.125)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		auto f1 = FixedPoint16q8_t(16);
+		auto f2 = FixedPoint16q8_t(0.125);
+		std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
+		std::cout << "f2 = FixedPoint16q8_t(0.125)" << std::endl;
+		std::cout << "11. f1 - f2 == 15.875 ->true?";
+		if (double(f1 - f2) == 15.875)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		auto f1 = FixedPoint16q8_t(16);
+		auto f2 = FixedPoint16q8_t(0.125);
+		std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
+		std::cout << "f2 = FixedPoint16q8_t(0.125)" << std::endl;
+		std::cout << "12. f1 * f2 == 2 ->true?";
+		if (double(f1 * f2) == 2)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		auto f1 = FixedPoint16q8_t(16);
+		auto f2 = FixedPoint16q8_t(0.125);
+		std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
+		std::cout << "f2 = FixedPoint16q8_t(0.125)" << std::endl;
+		std::cout << "13. f1 / f2 == 128 ->true?";
+		if (double(f1 / f2) == 128)
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		auto f1 = FixedPoint16q8_t(16);
+		std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
+		std::cout << "14. FixedPoint32q16_t(f1) == FixedPoint32q16_t(16) ->true?";
+		if (FixedPoint32q16_t(f1) == FixedPoint32q16_t(16))
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		std::cout << "15. FixedPoint32q16_t(Proportion16_t(3/4)) == FixedPoint32q16_t(0.75) ->true?";
+		if (FixedPoint16q15_t(Proportion16_t(3, 4)) == FixedPoint16q15_t(0.75))
+		{
+			std::cout << "...OK" << std::endl;
+			return 0;
+		}
+		else
+		{
+			std::cout << "...NG" << std::endl;
+			return 1;
+		}
+	},
+	[]()
+	{
+		std::cout << "16. FixedPoint16q8_t::Max() ? " << double(FixedPoint16q8_t::Max()) << std::endl;
+		return 0;
+	},
+	[]()
+	{
+		std::cout << "17. FixedPoint16q8_t::Min() ? " << double(FixedPoint16q8_t::Min()) << std::endl;
+		return 0;
+	},
+	[]()
+	{
+		std::cout << "18. FixedPoint16q8_t::Epsilon() ? " << double(FixedPoint16q8_t::Epsilon()) << std::endl;
+		return 0;
+	},
+};
 
 int main(int argc, char const *argv[])
 {
 	std::cout << "<--- FixedPoint --->" << std::endl;
-	if (argc < 2)
+	int test_index;
+	if (2 <= argc)
 	{
-		std::cerr << "E: At least 1 argument is required";
+		test_index = std::stoi(argv[1]);
+	}
+	else
+	{
+		std::cerr << "W: No test# specified" << std::endl;
+		std::cout << "Test#? ";
+		std::cin >> test_index;
+	}
+	if (0 < test_index && test_index <= tests.size() )
+	{
+		return tests[test_index - 1]();
+	}
+	else
+	{
+		std::cerr << "E: Invalid test index" << std::endl;
 		return 2;
 	}
-
-	auto test_index = std::stoi(argv[1]);
-	switch(test_index)
-	{
-		case 1: { return test1(); }
-		case 2: { return test2(); }
-		case 3: { return test3(); }
-		case 4: { return test4(); }
-		case 5: { return test5(); }
-		case 6: { return test6(); }
-		case 7: { return test7(); }
-		case 8: { return test8(); }
-		case 9: { return test9(); }
-		case 10: { return test10(); }
-		case 11: { return test11(); }
-		case 12: { return test12(); }
-		case 13: { return test13(); }
-		case 14: { return test14(); }
-		case 15: { return test15(); }
-		case 16: { return test16(); }
-		case 17: { return test17(); }
-		case 18: { return test18(); }
-		default:
-		{
-			std::cerr << "Invalid test index";
-			return 2;
-		}
-	}
 }
-
-int test1()
-{
-	std::cout << "1. Type FixedPoint8q7_t meets the traits IsNumericalType?";
-	if (Traits::IsNumericalType<FixedPoint8q7_t>)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test2()
-{
-	std::cout << "2. Type FixedPoint16q8_t meets the traits IsNumericalType?";
-	if (Traits::IsNumericalType<FixedPoint16q8_t>)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test3()
-{
-	std::cout << "3. Type FixedPoint16q15_t meets the traits IsNumericalType?";
-	if (Traits::IsNumericalType<FixedPoint16q15_t>)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test4()
-{
-	std::cout << "4. Type FixedPoint32q16_t meets the traits IsNumericalType?";
-	if (Traits::IsNumericalType<FixedPoint32q16_t>)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test5()
-{
-	std::cout << "5. Type FixedPoint32q31_t meets the traits IsNumericalType?";
-	if (Traits::IsNumericalType<FixedPoint32q31_t>)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test6()
-{
-	std::cout << "6. Type FixedPoint64q32_t meets the traits IsNumericalType?";
-	if (Traits::IsNumericalType<FixedPoint64q32_t>)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test7()
-{
-	std::cout << "7. Type FixedPoint64q63_t meets the traits IsNumericalType?";
-	if (Traits::IsNumericalType<FixedPoint64q63_t>)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test8()
-{
-	auto f1 = FixedPoint16q8_t(16);
-	std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
-	std::cout << "8. f1 == 16 ->true?";
-	if (double(f1) == 16)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test9()
-{
-	auto f2 = FixedPoint16q8_t(0.125);
-	std::cout << "f2 = FixedPoint16q8_t(0.125)" << std::endl;
-	std::cout << "9. f2 == 0.125 ->true?";
-	if (double(f2) == 0.125)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test10()
-{
-	auto f1 = FixedPoint16q8_t(16);
-	auto f2 = FixedPoint16q8_t(0.125);
-	std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
-	std::cout << "f2 = FixedPoint16q8_t(0.125)" << std::endl;
-	std::cout << "10. f1 + f2 == 16.125 ->true?";
-	if (double(f1 + f2) == 16.125)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test11()
-{
-	auto f1 = FixedPoint16q8_t(16);
-	auto f2 = FixedPoint16q8_t(0.125);
-	std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
-	std::cout << "f2 = FixedPoint16q8_t(0.125)" << std::endl;
-	std::cout << "11. f1 - f2 == 15.875 ->true?";
-	if (double(f1 - f2) == 15.875)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test12()
-{
-	auto f1 = FixedPoint16q8_t(16);
-	auto f2 = FixedPoint16q8_t(0.125);
-	std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
-	std::cout << "f2 = FixedPoint16q8_t(0.125)" << std::endl;
-	std::cout << "12. f1 * f2 == 2 ->true?";
-	if (double(f1 * f2) == 2)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test13()
-{
-	auto f1 = FixedPoint16q8_t(16);
-	auto f2 = FixedPoint16q8_t(0.125);
-	std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
-	std::cout << "f2 = FixedPoint16q8_t(0.125)" << std::endl;
-	std::cout << "13. f1 / f2 == 128 ->true?";
-	if (double(f1 / f2) == 128)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test14()
-{
-	auto f1 = FixedPoint16q8_t(16);
-	std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
-	std::cout << "14. FixedPoint32q16_t(f1) == FixedPoint32q16_t(16) ->true?";
-	if (FixedPoint32q16_t(f1) == FixedPoint32q16_t(16))
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test15()
-{
-	std::cout << "15. FixedPoint32q16_t(Proportion16_t(3/4)) == FixedPoint32q16_t(0.75) ->true?";
-	if (FixedPoint16q15_t(Proportion16_t(3, 4)) == FixedPoint16q15_t(0.75))
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-int test16()
-{
-	std::cout << "16. FixedPoint16q8_t::Max() ? " << double(FixedPoint16q8_t::Max()) << std::endl;
-	return 0;
-}
-
-int test17()
-{
-	std::cout << "17. FixedPoint16q8_t::Min() ? " << double(FixedPoint16q8_t::Min()) << std::endl;
-	return 0;
-}
-
-int test18()
-{
-	std::cout << "18. FixedPoint16q8_t::Epsilon() ? " << double(FixedPoint16q8_t::Epsilon()) << std::endl;
-	return 0;
-}
-
