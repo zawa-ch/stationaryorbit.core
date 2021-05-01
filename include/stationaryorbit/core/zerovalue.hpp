9749780b2b08c572848ae8fd96d2f2d41a9a1346
zawa-ch <satellite.2e1834097@gmail.com>
2021-05-01 17:46:57 +0900

    * ZetoValue_t
    コンストラクタを暗黙に宣言
    メンバ関数名の修正

--- include/stationaryorbit/core/zerovalue.hpp
+++ include/stationaryorbit/core/zerovalue.hpp
@@ -25,16 +25,11 @@ namespace zawa_ch::StationaryOrbit
 	///	他の型に変換可能な零値を表します。
 	struct ZeroValue_t final
 	{
-	public:
-		constexpr ZeroValue_t() = default;
-		constexpr ZeroValue_t(const ZeroValue_t&) = default;
-		constexpr ZeroValue_t(ZeroValue_t&&) = default;
-
 		constexpr operator bool() const { return 0; }
 		template<class = std::enable_if_t<std::negation_v<std::is_same<std::nullptr_t, void*>>>>
 		constexpr operator void*() const { return (void*)(0); }
 		constexpr operator std::nullptr_t() const { return nullptr; }
-		constexpr operator std::byte() const { return std::byte('\0'); }
+		constexpr operator std::byte() const { return std::byte{0}; }
 
 		constexpr ZeroValue_t operator+() const { return ZeroValue_t(); }
 		constexpr ZeroValue_t operator-() const { return ZeroValue_t(); }
@@ -59,10 +54,10 @@ namespace zawa_ch::StationaryOrbit
 		constexpr ZeroValue_t& operator<<=(const int&) { return *this; }
 		constexpr ZeroValue_t& operator>>=(const int&) { return *this; }
 
-		constexpr bool Equals(const ZeroValue_t&) const { return true; }
+		constexpr bool equals(const ZeroValue_t&) const { return true; }
 		constexpr bool operator==(const ZeroValue_t&) const { return true; }
 		constexpr bool operator!=(const ZeroValue_t&) const { return false; }
-		constexpr int Compare(const ZeroValue_t&) const { return 0; }
+		constexpr int compare(const ZeroValue_t&) const { return 0; }
 		constexpr bool operator<(const ZeroValue_t&) const { return false; }
 		constexpr bool operator>(const ZeroValue_t&) const { return false; }
 		constexpr bool operator<=(const ZeroValue_t&) const { return true; }
