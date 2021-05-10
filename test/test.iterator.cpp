6d8fc79d26abe3919a7615cdaf11c9ebaa13190c
zawa-ch <satellite.2e1834097@gmail.com>
2021-05-10 16:42:55 +0900

    * test.iterator

--- test/test.iterator.cpp
+++ test/test.iterator.cpp
@@ -19,22 +19,9 @@
 #include <iostream>
 #include <array>
 #include <functional>
+#include "test-helper.hpp"
 #include "stationaryorbit/core.iteration.hpp"
 using namespace zawa_ch::StationaryOrbit;
-
-constexpr int check_if(bool condition)
-{
-	if (condition)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
 
 class IncrementIterator
 {
