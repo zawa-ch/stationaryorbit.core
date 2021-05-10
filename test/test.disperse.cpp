8f2f2d484879290958546f344e564ea47ceacbb4
zawa-ch <satellite.2e1834097@gmail.com>
2021-05-10 16:36:34 +0900

    * test.disperse

--- test/test.disperse.cpp
+++ test/test.disperse.cpp
@@ -19,48 +19,10 @@
 #include <iostream>
 #include <array>
 #include <functional>
+#include "test-helper.hpp"
 #include "stationaryorbit/core.bitoperation.hpp"
 #include "stationaryorbit/core.utilities.hpp"
 using namespace zawa_ch::StationaryOrbit;
-
-constexpr int check_if(bool condition)
-{
-	if (condition)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
-
-template<size_t N>
-std::ostream& dump(std::ostream& stream, const std::array<std::byte, N>& data)
-{
-	auto flags = stream.flags();
-	auto width = stream.width();
-	auto fill = stream.fill();
-	size_t n = 0;
-	for(auto i: data)
-	{
-		if(16 <= n)
-		{
-			n = 0;
-			std::cout << std::endl;
-		}
-		stream.width(2);
-		stream.fill('0');
-		stream << std::hex << std::uppercase << uint16_t(i);
-		++n;
-	}
-	stream.flags(flags);
-	stream.width(width);
-	stream.fill(fill);
-	return stream;
-}
 
 std::array<std::function<int(void)>, 2> tests =
 {
