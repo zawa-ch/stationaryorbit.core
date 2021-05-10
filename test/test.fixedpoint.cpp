052baccd37fc756c8a4df050e3758aacc835ef5b
zawa-ch <satellite.2e1834097@gmail.com>
2021-05-10 16:42:28 +0900

    * test.fixedpoint

--- test/test.fixedpoint.cpp
+++ test/test.fixedpoint.cpp
@@ -19,154 +19,60 @@
 #include <iostream>
 #include <array>
 #include <functional>
+#include "test-helper.hpp"
 #include "stationaryorbit/core.numeral.hpp"
 using namespace zawa_ch::StationaryOrbit;
-
-constexpr int check_if(bool condition)
-{
-	if (condition)
-	{
-		std::cout << "...OK" << std::endl;
-		return 0;
-	}
-	else
-	{
-		std::cout << "...NG" << std::endl;
-		return 1;
-	}
-}
 
 std::array<std::function<int(void)>, 18> tests =
 {
 	[]()
 	{
 		std::cout << "1. Type FixedPoint8q7_t meets the traits IsNumericalType?";
-		if (Traits::IsNumericalType<FixedPoint8q7_t>)
-		{
-			std::cout << "...OK" << std::endl;
-			return 0;
-		}
-		else
-		{
-			std::cout << "...NG" << std::endl;
-			return 1;
-		}
+		return check_if(Traits::IsNumericalType<FixedPoint8q7_t>);
 	},
 	[]()
 	{
 		std::cout << "2. Type FixedPoint16q8_t meets the traits IsNumericalType?";
-		if (Traits::IsNumericalType<FixedPoint16q8_t>)
-		{
-			std::cout << "...OK" << std::endl;
-			return 0;
-		}
-		else
-		{
-			std::cout << "...NG" << std::endl;
-			return 1;
-		}
+		return check_if(Traits::IsNumericalType<FixedPoint16q8_t>);
 	},
 	[]()
 	{
 		std::cout << "3. Type FixedPoint16q15_t meets the traits IsNumericalType?";
-		if (Traits::IsNumericalType<FixedPoint16q15_t>)
-		{
-			std::cout << "...OK" << std::endl;
-			return 0;
-		}
-		else
-		{
-			std::cout << "...NG" << std::endl;
-			return 1;
-		}
+		return check_if(Traits::IsNumericalType<FixedPoint16q15_t>);
 	},
 	[]()
 	{
 		std::cout << "4. Type FixedPoint32q16_t meets the traits IsNumericalType?";
-		if (Traits::IsNumericalType<FixedPoint32q16_t>)
-		{
-			std::cout << "...OK" << std::endl;
-			return 0;
-		}
-		else
-		{
-			std::cout << "...NG" << std::endl;
-			return 1;
-		}
+		return check_if(Traits::IsNumericalType<FixedPoint32q16_t>);
 	},
 	[]()
 	{
 		std::cout << "5. Type FixedPoint32q31_t meets the traits IsNumericalType?";
-		if (Traits::IsNumericalType<FixedPoint32q31_t>)
-		{
-			std::cout << "...OK" << std::endl;
-			return 0;
-		}
-		else
-		{
-			std::cout << "...NG" << std::endl;
-			return 1;
-		}
+		return check_if(Traits::IsNumericalType<FixedPoint32q31_t>);
 	},
 	[]()
 	{
 		std::cout << "6. Type FixedPoint64q32_t meets the traits IsNumericalType?";
-		if (Traits::IsNumericalType<FixedPoint64q32_t>)
-		{
-			std::cout << "...OK" << std::endl;
-			return 0;
-		}
-		else
-		{
-			std::cout << "...NG" << std::endl;
-			return 1;
-		}
+		return check_if(Traits::IsNumericalType<FixedPoint64q32_t>);
 	},
 	[]()
 	{
 		std::cout << "7. Type FixedPoint64q63_t meets the traits IsNumericalType?";
-		if (Traits::IsNumericalType<FixedPoint64q63_t>)
-		{
-			std::cout << "...OK" << std::endl;
-			return 0;
-		}
-		else
-		{
-			std::cout << "...NG" << std::endl;
-			return 1;
-		}
+		return check_if(Traits::IsNumericalType<FixedPoint64q63_t>);
 	},
 	[]()
 	{
 		auto f1 = FixedPoint16q8_t(16);
 		std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
 		std::cout << "8. f1 == 16 ->true?";
-		if (double(f1) == 16)
-		{
-			std::cout << "...OK" << std::endl;
-			return 0;
-		}
-		else
-		{
-			std::cout << "...NG" << std::endl;
-			return 1;
-		}
+		return check_if(double(f1) == 16);
 	},
 	[]()
 	{
 		auto f2 = FixedPoint16q8_t(0.125);
 		std::cout << "f2 = FixedPoint16q8_t(0.125)" << std::endl;
 		std::cout << "9. f2 == 0.125 ->true?";
-		if (double(f2) == 0.125)
-		{
-			std::cout << "...OK" << std::endl;
-			return 0;
-		}
-		else
-		{
-			std::cout << "...NG" << std::endl;
-			return 1;
-		}
+		return check_if(double(f2) == 0.125);
 	},
 	[]()
 	{
@@ -175,16 +81,7 @@ std::array<std::function<int(void)>, 18> tests =
 		std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
 		std::cout << "f2 = FixedPoint16q8_t(0.125)" << std::endl;
 		std::cout << "10. f1 + f2 == 16.125 ->true?";
-		if (double(f1 + f2) == 16.125)
-		{
-			std::cout << "...OK" << std::endl;
-			return 0;
-		}
-		else
-		{
-			std::cout << "...NG" << std::endl;
-			return 1;
-		}
+		return check_if(double(f1 + f2) == 16.125);
 	},
 	[]()
 	{
@@ -193,16 +90,7 @@ std::array<std::function<int(void)>, 18> tests =
 		std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
 		std::cout << "f2 = FixedPoint16q8_t(0.125)" << std::endl;
 		std::cout << "11. f1 - f2 == 15.875 ->true?";
-		if (double(f1 - f2) == 15.875)
-		{
-			std::cout << "...OK" << std::endl;
-			return 0;
-		}
-		else
-		{
-			std::cout << "...NG" << std::endl;
-			return 1;
-		}
+		return check_if(double(f1 - f2) == 15.875);
 	},
 	[]()
 	{
@@ -211,16 +99,7 @@ std::array<std::function<int(void)>, 18> tests =
 		std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
 		std::cout << "f2 = FixedPoint16q8_t(0.125)" << std::endl;
 		std::cout << "12. f1 * f2 == 2 ->true?";
-		if (double(f1 * f2) == 2)
-		{
-			std::cout << "...OK" << std::endl;
-			return 0;
-		}
-		else
-		{
-			std::cout << "...NG" << std::endl;
-			return 1;
-		}
+		return check_if(double(f1 * f2) == 2);
 	},
 	[]()
 	{
@@ -229,46 +108,19 @@ std::array<std::function<int(void)>, 18> tests =
 		std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
 		std::cout << "f2 = FixedPoint16q8_t(0.125)" << std::endl;
 		std::cout << "13. f1 / f2 == 128 ->true?";
-		if (double(f1 / f2) == 128)
-		{
-			std::cout << "...OK" << std::endl;
-			return 0;
-		}
-		else
-		{
-			std::cout << "...NG" << std::endl;
-			return 1;
-		}
+		return check_if(double(f1 / f2) == 128);
 	},
 	[]()
 	{
 		auto f1 = FixedPoint16q8_t(16);
 		std::cout << "f1 = FixedPoint16q8_t(16)" << std::endl;
 		std::cout << "14. FixedPoint32q16_t(f1) == FixedPoint32q16_t(16) ->true?";
-		if (FixedPoint32q16_t(f1) == FixedPoint32q16_t(16))
-		{
-			std::cout << "...OK" << std::endl;
-			return 0;
-		}
-		else
-		{
-			std::cout << "...NG" << std::endl;
-			return 1;
-		}
+		return check_if(FixedPoint32q16_t(f1) == FixedPoint32q16_t(16));
 	},
 	[]()
 	{
 		std::cout << "15. FixedPoint32q16_t(Proportion16_t(3/4)) == FixedPoint32q16_t(0.75) ->true?";
-		if (FixedPoint16q15_t(Proportion16_t(3, 4)) == FixedPoint16q15_t(0.75))
-		{
-			std::cout << "...OK" << std::endl;
-			return 0;
-		}
-		else
-		{
-			std::cout << "...NG" << std::endl;
-			return 1;
-		}
+		return check_if(FixedPoint16q15_t(Proportion16_t(3, 4)) == FixedPoint16q15_t(0.75));
 	},
 	[]()
 	{
