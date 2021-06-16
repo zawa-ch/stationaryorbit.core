4b2e129ecfe44baa3a7ef8728154417e2d01486d
zawa-ch <satellite.2e1834097@gmail.com>
2021-06-16 17:12:12 +0900

    * InvalidOperationException リファクタリング
    usingによりコンストラクタを継承するよう変更

--- include/stationaryorbit/core/invalidoperation.hpp
+++ include/stationaryorbit/core/invalidoperation.hpp
@@ -23,17 +23,9 @@ namespace zawa_ch::StationaryOrbit
 {
 
 	///	無効な状態で呼び出されたメソッドに対してスローされる例外。
-	class InvalidOperationException
-		: public std::logic_error
+	class InvalidOperationException : public std::logic_error
 	{
-	public:
-
-		///	例外の説明を付け、このオブジェクトを初期化します。
-		explicit InvalidOperationException(const std::string& what_arg) noexcept;
-
-		///	例外の説明を付け、このオブジェクトを初期化します。
-		explicit InvalidOperationException(const char* what_arg) noexcept;
-
+		using std::logic_error::logic_error;
 	};
 
 }
