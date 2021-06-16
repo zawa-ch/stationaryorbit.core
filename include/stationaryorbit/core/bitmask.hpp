099e627bab773ce62d78f7ca7b6810a004c129f7
zawa-ch <satellite.2e1834097@gmail.com>
2021-06-16 16:45:22 +0900

    * BitMask テンプレート引数変更
    T -> Tp

--- include/stationaryorbit/core/bitmask.hpp
+++ include/stationaryorbit/core/bitmask.hpp
@@ -22,27 +22,27 @@
 #include "traits.hpp"
 namespace zawa_ch::StationaryOrbit
 {
-	///	ビット列をマスクする機能を提供します
+	///	@brief	ビット列をマスクする機能を提供します
 	///
 	///	マスクするためのビット列を保持し、マスクを用いたビット列の取得・変更を行います。
 	///	このクラスは継承できません。
 	///
-	///	@param	T
-	///	マスクのためのビット列の型。BitSequenceTypeの要件を満たす必要があります。
-	template<class T>
+	///	@param	Tp
+	///	マスクのためのビット列の型。型要件:BitSequenceType を満たす必要があります。
+	template<class Tp>
 	class BitMask final
 	{
-		static_assert(Traits::IsBitSequenceType<T>, "テンプレート型 T はビット列型である必要があります。");
+		static_assert(Traits::IsBitSequenceType<Tp>, "テンプレート型 Tp はビット列型である必要があります。");
 	public:
 		///	このオブジェクトのマスク値。
-		T mask;
+		Tp mask;
 
 		///	規定の @a BitMask を構築します。
 		constexpr BitMask() : mask() {}
 		///	マスク値を指定して @a BitMask を構築します。
 		///	@param	value
 		///	マスクを指定したビット列。
-		explicit constexpr BitMask(const T& value) noexcept : mask(value) {}
+		explicit constexpr BitMask(const Tp& value) noexcept : mask(value) {}
 
 		///	このマスクが開始される(最初にビットが1となる)、LSBから数えたビット数を取得します。
 		constexpr std::size_t begin_index() const noexcept { return get_begin_index(mask); }
@@ -53,50 +53,50 @@ namespace zawa_ch::StationaryOrbit
 		///	入力をマスク値に従ってマスクします。
 		///	@param	source
 		///	値を取得するソース。
-		constexpr T get_from(const T& source) const noexcept { return source & mask; }
+		constexpr Tp get_from(const Tp& source) const noexcept { return source & mask; }
 		///	入力をマスク値に従って書き込みます。
 		///	@param	destination
 		///	値の設定先。
 		///	@param	value
 		///	書き込みを行う値。
-		constexpr T set_to(const T& destination, const T& value) const noexcept { return (destination & ~mask) | (value & mask); }
+		constexpr Tp set_to(const Tp& destination, const Tp& value) const noexcept { return (destination & ~mask) | (value & mask); }
 		///	入力をマスク値に従ってマスクし、LSBに詰めます。
 		///	@param	source
 		///	値を取得するソース。
-		constexpr T get_aligned_from(const T& source) const noexcept { return get_from(source) >> get_begin_index(mask); }
+		constexpr Tp get_aligned_from(const Tp& source) const noexcept { return get_from(source) >> get_begin_index(mask); }
 		///	LSBに詰められた入力をマスク値に従って書き込みます。
 		///	@param	destination
 		///	値の設定先。
 		///	@param	value
 		///	書き込みを行う値。
-		constexpr T set_aligned_to(const T& destination, const T& value) const noexcept { return set_to(destination, value << get_begin_index(mask)); }
+		constexpr Tp set_aligned_to(const Tp& destination, const Tp& value) const noexcept { return set_to(destination, value << get_begin_index(mask)); }
 
-		constexpr BitMask<T> operator~() const noexcept { return BitMask<T>(~mask); }
-		constexpr BitMask<T> operator|(const BitMask<T>& other) const noexcept { return BitMask<T>(mask | other.mask); }
-		constexpr BitMask<T> operator&(const BitMask<T>& other) const noexcept { return BitMask<T>(mask & other.mask); }
-		constexpr BitMask<T> operator^(const BitMask<T>& other) const noexcept { return BitMask<T>(mask ^ other.mask); }
-		constexpr BitMask<T>& operator|=(const BitMask<T>& other) noexcept { return *this = BitMask<T>(mask | other.mask); }
-		constexpr BitMask<T>& operator&=(const BitMask<T>& other) noexcept { return *this = BitMask<T>(mask & other.mask); }
-		constexpr BitMask<T>& operator^=(const BitMask<T>& other) noexcept { return *this = BitMask<T>(mask ^ other.mask); }
+		constexpr BitMask<Tp> operator~() const noexcept { return BitMask<Tp>(~mask); }
+		constexpr BitMask<Tp> operator|(const BitMask<Tp>& other) const noexcept { return BitMask<Tp>(mask | other.mask); }
+		constexpr BitMask<Tp> operator&(const BitMask<Tp>& other) const noexcept { return BitMask<Tp>(mask & other.mask); }
+		constexpr BitMask<Tp> operator^(const BitMask<Tp>& other) const noexcept { return BitMask<Tp>(mask ^ other.mask); }
+		constexpr BitMask<Tp>& operator|=(const BitMask<Tp>& other) noexcept { return *this = BitMask<Tp>(mask | other.mask); }
+		constexpr BitMask<Tp>& operator&=(const BitMask<Tp>& other) noexcept { return *this = BitMask<Tp>(mask & other.mask); }
+		constexpr BitMask<Tp>& operator^=(const BitMask<Tp>& other) noexcept { return *this = BitMask<Tp>(mask ^ other.mask); }
 
 		///	他の @a BitMask との等価比較を行います。
 		///	@param	other
 		///	比較対象となるオブジェクト。
-		constexpr bool equals(const BitMask<T>& other) const noexcept { return mask == other.mask; }
-		constexpr bool operator==(const BitMask<T>& other) const noexcept { return equals(other); }
-		constexpr bool operator!=(const BitMask<T>& other) const noexcept { return !equals(other); }
+		constexpr bool equals(const BitMask<Tp>& other) const noexcept { return mask == other.mask; }
+		constexpr bool operator==(const BitMask<Tp>& other) const noexcept { return equals(other); }
+		constexpr bool operator!=(const BitMask<Tp>& other) const noexcept { return !equals(other); }
 	private:
 		///	指定された値の開始ビットのインデックスを取得します。
-		static constexpr std::size_t get_begin_index(const T& value, const T& start = 0) noexcept
+		static constexpr std::size_t get_begin_index(const Tp& value, const Tp& start = 0) noexcept
 		{
-			const size_t length = bitwidth<T>;
+			const size_t length = bitwidth<Tp>;
 			for (std::size_t i = start; i < length; i++) { if ((value & (1 << i)) != 0) { return i; } }
 			return length;
 		}
 		///	指定された値の終端ビットのインデックスを取得します。
-		static constexpr std::size_t get_end_index(const T& value, const T& start = std::numeric_limits<T>::max()) noexcept
+		static constexpr std::size_t get_end_index(const Tp& value, const Tp& start = std::numeric_limits<Tp>::max()) noexcept
 		{
-			const size_t length = bitwidth<T>;
+			const size_t length = bitwidth<Tp>;
 			for (std::size_t i = ((start < length)?start:length); 0 < i; i--) { if ((value & (1 << (i - 1))) != 0) { return i; } }
 			return length;
 		}
