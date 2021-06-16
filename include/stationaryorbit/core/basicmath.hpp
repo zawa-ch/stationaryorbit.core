7be1e1b7808d96b5dcf3f24a1a3f2e11f38bf060
zawa-ch <satellite.2e1834097@gmail.com>
2021-06-16 15:34:43 +0900

    * basicmath ドキュメント更新

--- include/stationaryorbit/core/basicmath.hpp
+++ include/stationaryorbit/core/basicmath.hpp
@@ -25,6 +25,7 @@
 #include "algorithms.hpp"
 namespace zawa_ch::StationaryOrbit
 {
+	///	単純な数学の演算をサポートします
 	class BasicMathematics
 	{
 	private:
@@ -59,48 +60,62 @@ namespace zawa_ch::StationaryOrbit
 		template<class Tp> static constexpr bool is_std_round_callable = IsStdRoundCallableImpl<Tp>::value;
 		template<class Tp> static constexpr bool has_sqrt = HasSqrtImpl<Tp>::value;
 	public:
+		///	指定された値の絶対値を求めます
 		template<class Tp>
 		[[nodiscard]] static constexpr std::enable_if_t<Traits::IsNumericalType<Tp>, Tp> abstract(const Tp& value) noexcept
 		{
 			return ( value < Tp(0) )?(-value):(value);
 		}
+		///	指定された値の絶対値を求めます
 		template<class Tp, class = decltype( std::abs( std::declval<Tp&>() ))>
 		[[nodiscard]] static constexpr Tp abstract(const Tp& value) noexcept { return std::abs(value); }
 
-		///	この値の平方根を取得します。
+		///	指定された値の平方根を求めます
 		template<class Tp, std::enable_if_t<(!is_std_sqrt_callable<Tp>) && (!has_sqrt<Tp>), int> = 0>
 		[[nodiscard]] static constexpr Tp square_root(const Tp& value) noexcept { return Algorithms::square_root<Tp>(value); }
+		///	指定された値の平方根を求めます
 		template<class Tp, std::enable_if_t<(is_std_sqrt_callable<Tp>), int> = 0>
 		[[nodiscard]] static constexpr Tp square_root(const Tp& value) noexcept { return std::sqrt(value); }
+		///	指定された値の平方根を求めます
 		template<class Tp, std::enable_if_t<(!is_std_sqrt_callable<Tp>) && (has_sqrt<Tp>), int> = 0>
 		[[nodiscard]] static constexpr decltype( std::declval<const Tp&>().Sqrt() ) square_root(const Tp& value) noexcept { return value.Sqrt(); }
 
+		///	指定された値を指定された値より大きい整数に丸めます
 		template<class Tp, std::enable_if_t<(Traits::IsIntegralType<Tp>), int> = 0>
 		[[nodiscard]] static constexpr Tp ceil(const Tp& value) { return value; }
+		///	指定された値を指定された値より大きい整数に丸めます
 		template<class Tp, std::enable_if_t<!(Traits::IsIntegralType<Tp>) && (is_std_ceil_callable<Tp>), int> = 0>
 		[[nodiscard]] static constexpr Tp ceil(const Tp& value) { return std::ceil(value); }
 
+		///	指定された値を指定された値より小さい整数に丸めます
 		template<class Tp, std::enable_if_t<(Traits::IsIntegralType<Tp>), int> = 0>
 		[[nodiscard]] static constexpr Tp floor(const Tp& value) { return value; }
+		///	指定された値を指定された値より小さい整数に丸めます
 		template<class Tp, std::enable_if_t<!(Traits::IsIntegralType<Tp>) && (is_std_floor_callable<Tp>), int> = 0>
 		[[nodiscard]] static constexpr Tp floor(const Tp& value) { return std::floor(value); }
 
+		///	指定された値を0に近い整数に丸めます
 		template<class Tp, std::enable_if_t<(Traits::IsIntegralType<Tp>), int> = 0>
 		[[nodiscard]] static constexpr Tp truncate(const Tp& value) { return value; }
+		///	指定された値を0に近い整数に丸めます
 		template<class Tp, std::enable_if_t<!(Traits::IsIntegralType<Tp>) && (is_std_trunc_callable<Tp>), int> = 0>
 		[[nodiscard]] static constexpr Tp truncate(const Tp& value) { return std::trunc(value); }
 
+		///	指定された値を最も近い整数に丸めます
 		template<class Tp, std::enable_if_t<(Traits::IsIntegralType<Tp>), int> = 0>
 		[[nodiscard]] static constexpr Tp round(const Tp& value) { return value; }
+		///	指定された値を最も近い整数に丸めます
 		template<class Tp, std::enable_if_t<!(Traits::IsIntegralType<Tp>) && (is_std_round_callable<Tp>), int> = 0>
 		[[nodiscard]] static constexpr Tp round(const Tp& value) { return std::round(value); }
 
+		///	剰余を求めます
 		template<class Tp>
 		[[nodiscard]] static constexpr std::enable_if_t<Traits::IsNumericalType<Tp>, Tp> mod(const Tp& left, const Tp& right)
 		{
 			// TODO: フォールバック実装の実装
 			throw NotImplementedException();
 		}
+		///	剰余を求めます
 		template<class Tp, class = decltype( std::fmod( std::declval<Tp&>(), std::declval<Tp&>() ))>
 		[[nodiscard]] static constexpr Tp mod(const Tp& value) noexcept { return std::fmod(value); }
 	};
