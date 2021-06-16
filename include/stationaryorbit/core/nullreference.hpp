ac5afd4c51accb4634b209d37f90ef53ec565e77
zawa-ch <satellite.2e1834097@gmail.com>
2021-06-16 17:12:32 +0900

    * NullReferenceException リファクタリング
    usingによりコンストラクタを継承するよう変更

--- include/stationaryorbit/core/nullreference.hpp
+++ include/stationaryorbit/core/nullreference.hpp
@@ -23,17 +23,9 @@ namespace zawa_ch::StationaryOrbit
 {
 
 	///	無効な参照を逆参照しようとした場合にスローされる例外。
-	class NullReferenceException
-		: public std::logic_error
+	class NullReferenceException : public std::logic_error
 	{
-	public:
-
-		///	例外の説明を付け、このオブジェクトを初期化します。
-		explicit NullReferenceException(const std::string& what_arg) noexcept;
-
-		///	例外の説明を付け、このオブジェクトを初期化します。
-		explicit NullReferenceException(const char* what_arg) noexcept;
-
+		using std::logic_error::logic_error;
 	};
 
 }
