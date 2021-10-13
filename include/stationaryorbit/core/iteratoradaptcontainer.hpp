22aa3394aed44e68bddd9ab0191ee741c85dc107
zawa-ch <satellite.2e1834097@gmail.com>
2021-10-13 14:17:45 +0900

    * IteratorAdaptContainer

--- include/stationaryorbit/core/iteratoradaptcontainer.hpp
+++ include/stationaryorbit/core/iteratoradaptcontainer.hpp
@@ -26,22 +26,22 @@ namespace zawa_ch::StationaryOrbit
 	///	このライブラリで使用されるイテレータをC++範囲ベースforで使用できるようにします。
 	///	@param	T
 	///	変換するイテレータの型。 @a IteratorTraits::is_sequencial_order_iterator を満たす必要があります。
-	template<class T, class = void>
+	template<typename T, typename = void>
 	class IteratorAdaptContainer
 	{
-		static_assert(SequencialOrderIteratorTraits::is_sequencial_order_iterator<T>, "テンプレート型 T はIteratorTraits::IsSequencialOrderIteratorを満たす必要があります。");
+		static_assert(SequencialOrderIteratorTraits::is_sequencial_order_iterator<T>, "テンプレート型 T は 型要件:SequencialOrderIterator を満たす必要があります。");
 	public:
-		typedef typename T::ValueType value_type;
-		typedef typename T::ValueType& reference;
-		typedef const typename T::ValueType& const_reference;
+		typedef SequencialOrderIteratorTraits::ValueType<T> value_type;
+		typedef SequencialOrderIteratorTraits::ValueType<T> reference;
+		typedef const SequencialOrderIteratorTraits::ValueType<T> const_reference;
 		typedef IteratorAdapter<T> iterator;
 		typedef IteratorAdapter<T> const_iterator;
-		typedef typename SequencialOrderIteratorTraits::IteratorDiff difference_type;
+		typedef SequencialOrderIteratorTraits::IteratorDiff difference_type;
 		typedef size_t size_type;
 	private:
 		T _itr;
 	public:
-		explicit constexpr IteratorAdaptContainer(const T& itr) : _itr(itr) {}
+		constexpr explicit IteratorAdaptContainer(const T& iterator) : _itr(iterator) {}
 
 		///	最初の要素を指すイテレータを取得します。
 		constexpr iterator begin() const { auto result = _itr; result.reset(); return iterator(result); }
@@ -53,15 +53,15 @@ namespace zawa_ch::StationaryOrbit
 		constexpr const_iterator cend() const { return end(); }
 	};
 
-	template<class T> class IteratorReverseAdaptContainer;
+	template<typename T> class IteratorReverseAdaptContainer;
 
 	///	このライブラリで使用されるイテレータをC++範囲ベースforで使用できるようにします。
 	///	@param	T
 	///	変換するイテレータの型。 @a IteratorTraits::is_bidirectional_order_iterator を満たす場合の拡張が存在します。
-	template<class T>
+	template<typename T>
 	class IteratorAdaptContainer<T, std::enable_if_t<BidirectionalOrderIteratorTraits::is_bidirectional_order_iterator<T>>>
 	{
-		static_assert(BidirectionalOrderIteratorTraits::is_bidirectional_order_iterator<T>, "テンプレート型 T はIteratorTraits::IsBidirectionalOrderIteratorを満たす必要があります。");
+		static_assert(BidirectionalOrderIteratorTraits::is_bidirectional_order_iterator<T>, "テンプレート型 T は 型要件:BidirectionalOrderIterator を満たす必要があります。");
 	public:
 		typedef typename T::ValueType value_type;
 		typedef typename T::ValueType& reference;
@@ -78,7 +78,7 @@ namespace zawa_ch::StationaryOrbit
 		explicit constexpr IteratorAdaptContainer(const T& itr) : _itr(itr) {}
 
 		///	イテレータの方向を反転します。
-		constexpr IteratorReverseAdaptContainer<T> Reverce() const { return IteratorReverseAdaptContainer(_itr); }
+		constexpr IteratorReverseAdaptContainer<T> reverse() const { return IteratorReverseAdaptContainer(_itr); }
 
 		///	最初の要素を指すイテレータを取得します。
 		constexpr iterator begin() const { auto result = _itr; result.reset(); return iterator(result); }
@@ -97,6 +97,7 @@ namespace zawa_ch::StationaryOrbit
 		///	最後の要素を指す逆イテレータを取得します。
 		constexpr reverse_const_iterator crend() const { return rend(); }
 	};
+
 	///	このライブラリで使用される逆方向のイテレータをC++範囲ベースforで使用できるようにします。
 	template<class T>
 	class IteratorReverseAdaptContainer
